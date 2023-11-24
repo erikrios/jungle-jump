e939e89404930603bc7b9ecf7e587c012a41d4d8
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-24 11:37:15 +0700

    Adding door scene

--- levels/level_base.gd
+++ levels/level_base.gd
@@ -9,6 +9,8 @@ signal score_changed
 
 var item_scene := load("res://items/item.tscn")
 
+var door_scene := load("res://items/door.tscn")
+
 var score := 0: set = set_score
 
 func set_score(value: int) -> void:
@@ -32,16 +34,23 @@ func spawn_items() -> void:
 	for cell in item_cells:
 		var data = $Items.get_cell_tile_data(0, cell)
 		var type = data.get_custom_data("type")
-		var item_type := Item.ItemType.CHERRY if type == "cherry" else Item.ItemType.GEM
-		var item = item_scene.instantiate()
-		add_child(item)
-		item.init(item_type	, $Items.map_to_local(cell))
-		item.picked_up.connect(self._on_item_picked_up)
+		if type == "door":
+			var door = door_scene.instantiate()
+			add_child(door)
+			door.position = $Items.map_to_local(cell)
+			door.body_entered.connect(self._on_door_entered)
+		else:
+			var item_type := Item.ItemType.CHERRY if type == "cherry" else Item.ItemType.GEM
+			var item = item_scene.instantiate()
+			add_child(item)
+			item.init(item_type	, $Items.map_to_local(cell))
+			item.picked_up.connect(self._on_item_picked_up)
 	
 func _on_item_picked_up() -> void:
 	score += 1
 
-
+func _on_door_entered(body: PhysicsBody2D) -> void:
+	GameState.next_level()
 
 func _on_player_died() -> void:
 	GameState.restart()
